def5421819e733450b0898728b34e053941cdcf4
Mahfuzul Hasan <mahfuzulh14@gmail.com>
2024-10-25 21:16:14 +0600

    ALL FUNCTIONS ADDED

--- TokenMarketPlace.sol
+++ TokenMarketPlace.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeERC20,IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {SafeMath} from"@openzeppelin/contracts/utils/math/SafeMath.sol";
 import{Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 // import "hardhat/console.sol";
 
 contract TokenMarketPlace is Ownable {
-    
+
 using SafeERC20 for IERC20;
 using SafeMath for uint256;
 
